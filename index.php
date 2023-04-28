fc6776e2aa7239eef3094a4bf3d148de680c6f5c
freestyle37 <mapryaz@gmail.com>
2023-04-28 21:25:07 +0600

    tetris

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico">
+    <script src="./functions.js" charset="utf-8"></script>
+    <title>Lesson</title>
+  </head>
+  <body>
+
+<p>Focus the IFrame first (e.g. by clicking in it), then try pressing some keys.</p>
+<p id="log"></p>
+<?php echo 'zdarova'; ?>
+
+  <style>
+
+  </style>
+
+
+  </body>
+</html>
