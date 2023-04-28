53bf51d9c8b8e6a01d824e54513f00d8b23a784a
freestyle37 <mapryaz@gmail.com>
2023-04-28 21:31:44 +0600

    tetris boiler

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
   <head>
     <meta charset="UTF-8">
     <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="styles.css">
     <script src="./functions.js" charset="utf-8"></script>
     <title>Lesson</title>
   </head>
   <body>
 
-<p>Focus the IFrame first (e.g. by clicking in it), then try pressing some keys.</p>
-<p id="log"></p>
-<?php echo 'zdarova'; ?>
+  <div class="dpk"></div>
 
   <style>
 
